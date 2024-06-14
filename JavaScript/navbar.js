b08e699f26f281cbff2724231dcad0cbdc158ee7
Sprickels <108860809+Sprickels@users.noreply.github.com>
2024-06-14 10:51:32 +0700

    Navbar barebone update

---
+++ JavaScript/navbar.js
@@ -0,0 +1,13 @@
+const currentPath = window.location.pathname.split("/").pop();
+
+const navLinks = document.querySelectorAll('.nav_links li a')
+
+navLinks.forEach(link => {
+    if(link.getAttribute('href') === currentPath){
+        link.classList.add('active');
+    } else {
+        link.classList.remove('active');
+    }
+});
+
+  
