7d128e3e8cecc6e3e8a8d5a0304faafc3b37c60e
Kennard Adrian <Kennardkhouw@gmail.com>
2024-06-17 23:07:59 +0700

    update kuliner

---
+++ JavaScript/kulinerpage.js
@@ -0,0 +1,117 @@
+var detail1 = document.getElementById("asap");
+var opnbtn1 = document.getElementById("asapOpen");
+var clsbtn1 = document.getElementById("asapClose");
+
+opnbtn1.onclick = function(){
+    detail1.style.display = "block";
+}
+
+clsbtn1.onclick = function(){
+    detail1.style.display = "none";
+} 
+
+window.onclick = function(event) {
+    if (event.target == detail1) {
+      detail1.style.display = "none";
+    }
+}
+
+var detail2 = document.getElementById("papeda");
+var opnbtn2 = document.getElementById("papedaOpen");
+var clsbtn2 = document.getElementById("papedaClose");
+
+
+opnbtn2.onclick = function(){
+    detail2.style.display = "block";
+}
+
+clsbtn2.onclick = function(){
+    detail2.style.display = "none";
+} 
+
+window.onclick = function(event) {
+    if (event.target == detail2) {
+      detail2.style.display = "none";
+    }
+}
+
+
+var detail3 = document.getElementById("udang");
+var opnbtn3 = document.getElementById("udangOpen");
+var clsbtn3 = document.getElementById("udangClose");
+
+
+opnbtn3.onclick = function(){
+    detail3.style.display = "block";
+}
+
+clsbtn3.onclick = function(){
+    detail3.style.display = "none";
+} 
+
+window.onclick = function(event) {
+    if (event.target == detail3) {
+      detail3.style.display = "none";
+    }
+}
+
+
+var detail4 = document.getElementById("SP");
+var opnbtn4 = document.getElementById("SPOpen");
+var clsbtn4 = document.getElementById("SPClose");
+
+
+opnbtn4.onclick = function(){
+    detail4.style.display = "block";
+}
+
+clsbtn4.onclick = function(){
+    detail4.style.display = "none";
+} 
+
+window.onclick = function(event) {
+    if (event.target == detail4) {
+      detail4.style.display = "none";
+    }
+}
+
+
+var detail5 = document.getElementById("SC");
+var opnbtn5 = document.getElementById("SCOpen");
+var clsbtn5 = document.getElementById("SCClose");
+
+
+opnbtn5.onclick = function(){
+    detail5.style.display = "block";
+}
+
+clsbtn5.onclick = function(){
+    detail5.style.display = "none";
+} 
+
+window.onclick = function(event) {
+    if (event.target == detail5) {
+      detail5.style.display = "none";
+    }
+}
+
+
+var detail6 = document.getElementById("YBF");
+var opnbtn6 = document.getElementById("YBFOpen");
+var clsbtn6 = document.getElementById("YBFClose");
+
+
+opnbtn6.onclick = function(){
+    detail6.style.display = "block";
+}
+
+clsbtn6.onclick = function(){
+    detail6.style.display = "none";
+} 
+
+window.onclick = function(event) {
+    if (event.target == detail6) {
+      detail6.style.display = "none";
+    }
+}
+
